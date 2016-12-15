f4262aa83ac424f0b23e6b36922e8a7d223939aa
Grant Smith <smithge87@gmail.com>
2016-12-15 12:02:28 -0600

    mvp reached, cleaning code, testing, and working with sort functions

--- CustomList/CustomList/CustomList.cs
+++ CustomList/CustomList/CustomList.cs
@@ -7,21 +7,21 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-   public class CustomList<T> : IEnumerable 
+    public class CustomList<T> : IEnumerable
     {
         T value;
-        public int count=0;
+        public int count = 0;
         public T[] customArray;
         public CustomList()
         {
-             customArray = new T[0];
+            customArray = new T[0];
         }
         public void Add(T addToArray)
         {
             if (customArray.Count() >= 0)
-            value = addToArray;
+                value = addToArray;
             T[] array = new T[customArray.Length + 1];
-            for  (int i =0; i < customArray.Length; i++)
+            for (int i = 0; i < customArray.Length; i++)
             {
                 array[i] = customArray[i];
             }
@@ -41,16 +41,16 @@ namespace CustomList
             {
                 value = removeFromArray;
                 T[] array = new T[customArray.Length - 1];
-                int j= 0;
+                int j = 0;
                 for (int i = 0; i < customArray.Count(); i++)
                 {
-                    if (customArray[i].Equals(value) && j==0 )
+                    if (customArray[i].Equals(value) && j == 0)
                     {
                         j++;
                     }
                     else
                     {
-                        array[i-j] = customArray[i];
+                        array[i - j] = customArray[i];
                     }
                 }
                 customArray = array;
@@ -60,7 +60,6 @@ namespace CustomList
             {
                 return false;
             }
-
         }
         public int Count
         {
@@ -72,14 +71,13 @@ namespace CustomList
                     {
                         count++;
                     }
-                    return count;                      
+                    return count;
                 }
                 else
                 {
                     return 0;
                 }
             }
-
         }
         public IEnumerator GetEnumerator()
         {
@@ -88,40 +86,40 @@ namespace CustomList
                 yield return customArray[i];
             }
         }
-        public static CustomList<T> operator+ (CustomList<T> one , CustomList<T> two)
+        public static CustomList<T> operator +(CustomList<T> one, CustomList<T> two)
         {
             T[] array = new T[one.customArray.Length + two.customArray.Length];
             for (int i = 0; i < one.customArray.Length; i++)
-                if (one.customArray.Length > 0) 
+                if (one.customArray.Length > 0)
                 {
                     array[i] = one.customArray[i];
                 }
-            for (int i = one.customArray.Length; i < (one.customArray.Length+two.customArray.Length);i++ )
-                if ((one.customArray.Length+two.customArray.Length) > one.customArray.Length)
+            for (int i = one.customArray.Length; i < (one.customArray.Length + two.customArray.Length); i++)
+                if ((one.customArray.Length + two.customArray.Length) > one.customArray.Length)
                 {
-                    array[i] = two.customArray[i-one.customArray.Length];
+                    array[i] = two.customArray[i - one.customArray.Length];
                 }
             one.customArray = array;
             return one;
         }
-        public static CustomList<T> operator- (CustomList<T> one , CustomList<T> two )
+        public static CustomList<T> operator -(CustomList<T> one, CustomList<T> two)
         {
             int j = 0;
             T[] array = new T[one.customArray.Length];
             for (int i = 0; i < one.customArray.Length; i++)
                 for (int k = 0; k < two.customArray.Length; k++)
-                if(one.customArray[i].Equals(two.customArray[k]))
-                {   
-                    j++;
-                    k = two.customArray.Length;
-                }
-                else
-                {
-                    if (i != one.customArray.Length)
+                    if (one.customArray[i].Equals(two.customArray[k]))
+                    {
+                        j++;
+                        k = two.customArray.Length;
+                    }
+                    else
+                    {
+                        if (i != one.customArray.Length)
                         {
                             array[i - j] = one.customArray[i];
-                        }                                     
-                }
+                        }
+                    }
             T[] arrayTwo = new T[one.customArray.Length - j];
             for (int l = 0; l < array.Length; l++)
                 if (array[l] != null && (arrayTwo.Length > l))
@@ -131,56 +129,47 @@ namespace CustomList
             one.customArray = arrayTwo;
             return one;
         }
-        public  CustomList<T> Zip(CustomList<T>one, Func<T, T, T> func1)
+        public CustomList<T> Zip(CustomList<T> one, Func<T, T, T> func1)
         {
-         
             int l = 0;
             for (int i = 0; i < customArray.Length; i++)
                 if ((customArray[i] != null) && (one.customArray[i] != null))
                 {
                     l++; l++;
                 }
-            CustomList<T> three = new CustomList<T>();
+            CustomList<T> temporaryList = new CustomList<T>();
             T[] array = new T[l];
-            three.customArray = array;
-            T threecontent;
-            int k=0;
-            int j=1;
+            temporaryList.customArray = array;
+            T temporaryListContent;
+            int k = 0;
+            int j = 1;
             for (int i = 0; i < customArray.Length; i++)
-            if ((customArray[i] != null) && (one.customArray[i] != null))
-            {
-                    threecontent = customArray[i];
-                    three.customArray[i+k] = threecontent;
+                if ((customArray[i] != null) && (one.customArray[i] != null))
+                {
+                    temporaryListContent = customArray[i];
+                    temporaryList.customArray[i + k] = temporaryListContent;
                     k++;
-                    threecontent = one.customArray[i];
-                    three.customArray[i + j] = threecontent;
+                    temporaryListContent = one.customArray[i];
+                    temporaryList.customArray[i + j] = temporaryListContent;
                     j++;
                 }
-            return three;
+            return temporaryList;
         }
         public override string ToString()
         {
             string converted = "";
             foreach (T i in customArray)
-                {
+            {
                 converted += i;
-                }
+            }
             return converted;
         }
-        //public interface IComparable<in T> {};
-
         public static void exchange(CustomList<T> one, int m, int n)
         {
             T temporary = one.customArray[m];
             one.customArray[m] = one.customArray[n];
             one.customArray[n] = temporary;
         }
-        //public int CompareTo(T other)
-        //{
-        //    if (other == null) return 1;
-
-        //    return CompareTo(other);
-        //}
         public void BubbleSort(CustomList<T> one)
         {
             int i, j;
@@ -194,35 +183,28 @@ namespace CustomList
                 }
             }
         }
+        //public int Capacity { get; set; }
+        //DONE    public int Count { get; }
+        //DONE    public CustomList();
+        //DONE    public void Add(T item);
+        //public void Insert(int index, T item);
+        //DONE    public bool Remove(T item);
+        //DONE    public static SuperList<T> operator -(SuperList<T> list1, SuperList<T> list2);
+        //DONE    public static SuperList<T> operator +(SuperList<T> list1, SuperList<T> list2);
+        //DONE    public IEnumerator<T> GetEnumerator();
+        //IEnumerator IEnumerable.GetEnumerator();
 
-
-
-
-            //public int Capacity { get; set; }
-            //DONE    public int Count { get; }
-            //DONE    public CustomList();
-            //DONE    public void Add(T item);
-            //public void Insert(int index, T item);
-            //DONE    public bool Remove(T item);
-            //DONE    public static SuperList<T> operator -(SuperList<T> list1, SuperList<T> list2);
-            //DONE    public static SuperList<T> operator +(SuperList<T> list1, SuperList<T> list2);
-            //DONE    public IEnumerator<T> GetEnumerator();
-            //IEnumerator IEnumerable.GetEnumerator();
-
-            //        The built-in List<T> class is a generic class that acts as a wrapper over the array class. You cannot use built-in List or Array methods.
-            //DONE    TESTED    (10 points): As a developer, I want to use a custom-built list class that stores its values in an array, so that I can store any data type in my collection.
-            //DONE    TESTED    (10 points): As a developer, I want the ability to add an object to an instance of my custom-built list class.
-            //DONE    TESTED    (10 points): As a developer, I want the ability to remove an object from an instance of my custom-built list class.
-            //DONE    (10 points): As a developer, I want the custom list class to be iterable.
-            //DONE    (10 points): As a developer, I want to be able to override the ToString method that converts the contents of the custom list to a string.
-            //DONE    TESTED    (10 points): As a developer, I want to be able to overload the + operator, so that I can add two instances of the custom list class together.
-            //DONE    TESTED    (10 points): As a developer, I want to be able to overload the – operator, so that I can subtract one instance of a custom list class from another instance of a custom list class.
-            //DONE    TESTED    (10 points): As a developer, I want a Count property implemented on the custom-built list class, so that I can get a count of the number of elements in my custom list class instance.
-            //DONE    (10 points): As a developer, I want the ability to Enumerable.Zip() two custom list class instances together.
-            //(10 points): As a developer, I want to use C# best practices, SOLID design principles, and good naming conventions on the project. 
-            //(20 points (2 points each)): As a developer, I want to create 10 unit tests for my project, so that I can ensure the functionality is working properly.
-            //(Bonus 5 points): As a developer, I want the ability to sort an instance of my custom-built list class. To be eligible for the bonus points, you may not use Array.Sort() that is already built in and you must tell us what sorting algorithm you used.
-        }
-
-
+        //        The built-in List<T> class is a generic class that acts as a wrapper over the array class. You cannot use built-in List or Array methods.
+        //DONE    TESTED    (10 points): As a developer, I want to use a custom-built list class that stores its values in an array, so that I can store any data type in my collection.
+        //DONE    TESTED    (10 points): As a developer, I want the ability to add an object to an instance of my custom-built list class.
+        //DONE    TESTED    (10 points): As a developer, I want the ability to remove an object from an instance of my custom-built list class.
+        //DONE    (10 points): As a developer, I want the custom list class to be iterable.
+        //DONE    (10 points): As a developer, I want to be able to override the ToString method that converts the contents of the custom list to a string.
+        //DONE    TESTED    (10 points): As a developer, I want to be able to overload the + operator, so that I can add two instances of the custom list class together.
+        //DONE    TESTED    (10 points): As a developer, I want to be able to overload the – operator, so that I can subtract one instance of a custom list class from another instance of a custom list class.
+        //DONE    TESTED    (10 points): As a developer, I want a Count property implemented on the custom-built list class, so that I can get a count of the number of elements in my custom list class instance.
+        //DONE    (10 points): As a developer, I want the ability to Enumerable.Zip() two custom list class instances together.
+        //(10 points): As a developer, I want to use C# best practices, SOLID design principles, and good naming conventions on the project. 
+        //(20 points (2 points each)): As a developer, I want to create 10 unit tests for my project, so that I can ensure the functionality is working properly.
+    }
 }
